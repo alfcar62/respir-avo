adf6f82ac27f79bb5468931c85c060a63a53004c
alfcar62 <alfonso.carlone@gmail.com>
2022-04-01 16:33:02 +0200

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		Graph graph = new Graph("misure.csv", "pm 2.5 (Plume AQI)", 10 , 850, 5);
+	}
+
+}
