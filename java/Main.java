edf61fca206b8278e470125d5eb1a90703887004
tzyvoski <OxyGen05@outlook.it>
2022-04-04 11:13:24 +0200

    Applicato cambiamenti

--- java/Main.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-	
-	public static void main(String args[]) {
-		Graph graph = new Graph("misure.csv", "pm 2.5 (Plume AQI)", 10 , 850, 5);
-	}
-
-}
